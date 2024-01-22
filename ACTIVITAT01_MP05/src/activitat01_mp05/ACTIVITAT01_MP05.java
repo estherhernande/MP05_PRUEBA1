5d568941febddf5fc1092537bac34a5aaec07c37
esthe <esthe@LAPTOP-8HOQD5UJ>
2024-01-22 20:50:12 +0100

    Programa Hola Mundo para actividad1 de MP05 de la UF2

---
+++ ACTIVITAT01_MP05/src/activitat01_mp05/ACTIVITAT01_MP05.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package activitat01_mp05;
+
+/**
+ *
+ * @author esthe
+ */
+public class ACTIVITAT01_MP05 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        
+        // TODO ESTO ES UNA PRUEBA PARA EJERCICIO DE MP05
+        System.out.println("Hola Mundo!");
+    }
+    
+}
+
